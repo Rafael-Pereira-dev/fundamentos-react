48549cd6884615d8448d9d6459c4b59bd1319852
Luciana <rafael.rpereira@al.infnet.edu.br>
2024-08-03 00:54:18 -0300

    aula 01 de fundamentos de react

---
+++ aula01/src/App.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <h2>Olá</h2>
+    </div>
+    
+  )
+}
+
+export default App;
